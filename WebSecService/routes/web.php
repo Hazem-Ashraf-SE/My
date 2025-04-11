c5b0a3e92ab22edc6fce4593e28519f82446e3ad
Hazem-Ashraf-SE <157379336+Hazem-Ashraf-SE@users.noreply.github.com>
2025-04-11 15:12:55 +0200

    WebSecCreation

---
+++ WebSecService/routes/web.php
@@ -0,0 +1,72 @@
+<?php
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Web\ProductsController;
+use App\Http\Controllers\Web\UsersController;
+use App\Http\Controllers\CustomerController;
+
+Route::get('register', [UsersController::class, 'register'])->name('register');
+Route::post('register', [UsersController::class, 'doRegister'])->name('do_register');
+Route::get('login', [UsersController::class, 'login'])->name('login');
+Route::post('login', [UsersController::class, 'doLogin'])->name('do_login');
+Route::get('logout', [UsersController::class, 'doLogout'])->name('do_logout');
+Route::get('users', [UsersController::class, 'list'])->name('users');
+Route::get('profile/{user?}', [UsersController::class, 'profile'])->name('profile');
+Route::get('users/edit/{user?}', [UsersController::class, 'edit'])->name('users_edit');
+Route::post('users/save/{user}', [UsersController::class, 'save'])->name('users_save');
+Route::get('users/delete/{user}', [UsersController::class, 'delete'])->name('users_delete');
+Route::get('users/edit_password/{user?}', [UsersController::class, 'editPassword'])->name('edit_password');
+Route::post('users/save_password/{user}', [UsersController::class, 'savePassword'])->name('save_password');
+
+
+
+Route::get('products', [ProductsController::class, 'list'])->name('products_list');
+Route::get('products/edit/{product?}', [ProductsController::class, 'edit'])->name('products_edit');
+Route::post('products/save/{product?}', [ProductsController::class, 'save'])->name('products_save');
+Route::get('products/delete/{product}', [ProductsController::class, 'delete'])->name('products_delete');
+
+Route::middleware(['auth'])->group(function () {
+    // Route for listing customers
+    Route::get('customers', [CustomerController::class, 'index'])->name('customers.index');
+
+    // Route to show a form to add credit to a customer
+    Route::get('customers/{customer}/add-credit', [CustomerController::class, 'showAddCreditForm'])->name('customers.addCreditForm');
+    Route::post('customers/{customer}/add-credit', [CustomerController::class, 'addCredit'])->name('customers.addCredit');
+
+    // Route for creating a new customer
+    Route::get('customers/create', [CustomerController::class, 'create'])->name('customers.create');
+    Route::post('customers', [CustomerController::class, 'store'])->name('customers.store');
+
+    // Route for editing a customer
+    Route::get('customers/{customer}/edit', [CustomerController::class, 'edit'])->name('customers.edit');
+    Route::put('customers/{customer}', [CustomerController::class, 'update'])->name('customers.update');
+
+    // Route for deleting a customer
+    Route::delete('customers/{customer}', [CustomerController::class, 'destroy'])->name('customers.destroy');
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/multable', function (Request $request) {
+    $j = $request->number??5;
+    $msg = $request->msg;
+    return view('multable', compact("j", "msg"));
+});
+
+Route::get('/even', function () {
+    return view('even');
+});
+
+Route::get('/prime', function () {
+    return view('prime');
+});
+
+Route::get('/test', function () {
+    return view('test');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
